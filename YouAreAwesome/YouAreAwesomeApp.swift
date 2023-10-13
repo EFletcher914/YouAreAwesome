a0b3482952d9d66079d7cb9c7b7cf7ddba1ffdf4
Eric Fletcher <insitefule@gmail.com>
2023-10-12 20:16:39 -0400

    Initial Commit

---
+++ YouAreAwesome/YouAreAwesomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  YouAreAwesomeApp.swift
+//  YouAreAwesome
+//
+//  Created by Eric Fletcher on 10/12/23.
+//
+
+import SwiftUI
+
+@main
+struct YouAreAwesomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
