3801f9336c1fa3bb1e8f242e80133787c1f4fe46
Eric Fletcher <insitefule@gmail.com>
2023-10-15 22:58:13 -0400

    updated the message label and image also created while loops and repeat while

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -13,8 +13,8 @@ struct ContentView: View {
     
     @State private var imageName = ""
     
-    @State private var imageNumber = 0
-    
+    @State private var lastMessageNumber = -1
+    @State private var lastImageNumber = -1
     var body: some View {
         
         
@@ -22,19 +22,7 @@ struct ContentView: View {
         
         VStack {
             
-            Image(imageName)
-                .resizable()
-                .scaledToFit()
-                .cornerRadius(30)
-                .shadow(radius: 30)
-                .padding()
             
-            
-            
-            
-            Spacer()
-            
-         
             Text(messageString)
                 .font(.largeTitle)
                 .fontWeight(.semibold)
@@ -47,49 +35,83 @@ struct ContentView: View {
                 .frame(maxWidth: .infinity)
                 .padding()
             
+            
+            
+            
+            
+            
+            Image(imageName)
+                .resizable()
+                .scaledToFit()
+                .cornerRadius(30)
+                .shadow(radius: 30)
+                .padding()
+            
+            
             Spacer()
             
-                
             
-          
-                Button("Show Message") {
-                 
+            
+            
+            Button("Show Message") {
+                
+                let messages = ["You Are Great!",
+                                "You Are Awesome!",
+                                "You can do it!",
+                                "Don't Stop Keep Going!",
+                                "Beileve In YourSelf!",
+                                "Quitting Is Not An Option!",
+                                "You Make Me Smile!"]
+                
+                
+                
+                // if messageNumber == laastMessageNumber {
+                //keep generating a new random messageNumber
+                // untik you get a messageNumber!= lastMessageNumber
+                // Set messageString to message[messageNumber]
+                // update the lastMessageNumber with messageNumber
+                
+                
+                
+                
+                // generate a random messagNumber to use as an index
+                
+                var messageNumber: Int
+                
+                repeat {
+                    messageNumber = Int.random(in: 0...messages.count-1)
+                } while messageNumber == lastMessageNumber
+                
+                messageString = messages[messageNumber]
+                lastMessageNumber = messageNumber
+                
+                // This restets the images to the beginning after 9
+                
+                
+                var imageNumber: Int
+                repeat {
+                    imageNumber = Int.random(in: 0...9)
                     
-                    let message1 = "You Are Awesome!"
-                    let message2 = "You Are Great"
-                  
-                  
-                    messageString = (messageString == message1 ? message2 : message1)
-                 
-
-                  
-                    
-                    // This restets the images to the beginning after 9
-                    imageName = "image\(imageNumber)"
-                    imageNumber = imageNumber + 1
-                 
-                    if imageNumber > 9 {
-                        imageNumber = 0
-                    }
-                   
-                    
-                    
+                }while imageNumber == lastImageNumber
+                imageName = "image\(imageNumber)"
+                lastImageNumber = imageNumber
+                
                     
                 }
-                .buttonStyle(.borderedProminent)
-                
-                .padding()
-              
-            .tint(Color("OceanBlue"))
-            
-        }
-    }
-}
-
-
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
-}
+                    .buttonStyle(.borderedProminent)
+                                             
+                    .padding()
+                                             
+                    .tint(Color("OceanBlue"))
+                                             
+                                             }
+                                             }
+                                             }
+                                             
+                                             
+                                             
+                                             struct ContentView_Previews: PreviewProvider {
+                    static var previews: some View {
+                        ContentView()
+                    }
+                }
